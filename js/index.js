5078f5f7bc5cc6615bf84c4f83e058cf1717ee7d
Devsk001 <srik3713@gmail.com>
2023-06-27 21:56:58 +0530

    FadeIn css ND Js Added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 console.log("Welcome to Devstella");
+
+//        Fade-In Js For Scroll evenet
+const fadeIns = document.querySelectorAll('.fade-in');
+
+function checkFadeIns() {
+  fadeIns.forEach(fadeIn => {
+    const fadeInTop = fadeIn.getBoundingClientRect().top;
+    const fadeInBottom = fadeIn.getBoundingClientRect().bottom;
+
+    if (fadeInTop < window.innerHeight && fadeInBottom >= 0) {
+      fadeIn.classList.add('active');
+    } else {
+      fadeIn.classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', checkFadeIns);
+
+//     move bottom to top animation
+
+
+//               end of Js
